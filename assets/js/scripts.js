44007bad2542b63e096e905da18e06614bb8973a
Nantet <renannantet31@gmail.com>
2025-01-11 16:06:28 -0300

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@
 const botaoEstouComSorte = document.querySelector('form [type="button"]');
 
 botaoEstouComSorte.addEventListener("click", () => {
-    location.href = "https://www.youtube.com/watch?v=FYaYtvmqP6c";
+    location.href = "https://doodles.google/";
 });
 
 // Selecionando o botão de menu
@@ -11,3 +11,20 @@ const menu = document.querySelector("#btn-menu");  // Use # para ID
 menu.addEventListener("click", () => {
     console.log("Clicou");
 });
+
+// Selecionando o botão "Pesquisa Google"
+const botaoPesquisaGoogle = document.querySelector('form [type="submit"]');
+
+botaoPesquisaGoogle.addEventListener("click", () => {
+
+    location.href = "https://doodles.google/"
+
+});
+
+const Gmail = document.querySelector('nav [class="link-desktop"]');
+
+Gmail.addEventListener("Click", () => {
+    location.href = "https://mail.google.com/mail/u/0/#inbox";
+});
+
+
